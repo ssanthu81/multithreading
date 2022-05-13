29854d0b18b5864801bb21c82572d7154f1f4a8c
ssanthu81 <101133033+ssanthu81@users.noreply.github.com>
2022-05-13 08:59:47 -0500

    Add files via upload

---
+++ multithreading/multithreading/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+class Profram
+{
+    static void test()
+    {
+        for (int i = 1; i <= 100; i++)
+            Console.WriteLine("test:" + i);
+    }
+    static void Main(string[] args)
+    {
+        Profram t = new Profram();
+        test();
+        Console.ReadLine();
+    }
+}
